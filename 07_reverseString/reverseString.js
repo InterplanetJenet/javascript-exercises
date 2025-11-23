f93e934b781b62952eaedb1c2a332059ffa81331
InterplanetJenet <17688052+InterplanetJenet@users.noreply.github.com>
2025-11-23 14:20:07 -0600

    Fix output for a single word BY adding function AND let string AND define string AND return a chain of the js built in methods split reverse join

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word){
 
+let string = "";{
+string = word;
+}
+{
+    return word.split('').reverse().join('');
 };
+}
 
 // Do not edit below this line
 module.exports = reverseString;
