90c208a8ba490268990f86ee55fe1af2c553ff47
InterplanetJenet <17688052+InterplanetJenet@users.noreply.github.com>
2025-11-23 17:52:46 -0600

    Clean and condense code BY replaceing function word with function string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,15 +1,5 @@
-const reverseString = function(word){
-
-let string = "";
-string = word;
-    if (word === null || word === undefined || word === ''){
-        return '';
-    }
-    else {
-    for (let i=0; i < word.length; i++) {
-        return word.split('').reverse().join('');
-}
-}
+const reverseString = function(string){
+  return string.split('').reverse().join('');
 }
 
 // Do not edit below this line
