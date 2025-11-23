b965175221cc9bde174f9ae167417feff3448f3a
InterplanetJenet <17688052+InterplanetJenet@users.noreply.github.com>
2025-11-23 16:25:20 -0600

    Output correctly for multiple words and punctuation BY Adding for loop AND unlocked the next two tests BY removing skip

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -2,10 +2,11 @@ const reverseString = function(word){
 
 let string = "";{
 string = word;
-}
+for (let i=0; i < word.length -1; i++)
 {
     return word.split('').reverse().join('');
-};
+}
+}
 }
 
 // Do not edit below this line
